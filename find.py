045bbecd568ab0a8d575e63fb7bb548d231c29a6
Philippa Richter <pips.richter@gmail.com>
2026-05-11 11:42:29 -0700

    more curation work

--- find.py
+++ find.py
@@ -10,20 +10,35 @@ import re
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('pattern', dtype=str)
+    parser.add_argument('pattern')
     parser.add_argument('--root', default='/groups/banfield/sequences/')
-    # parser.add_argument('--levels', default=2, dtype=int)
+    parser.add_argument('--root', default='/groups/banfield/scratch/projects/environmental/RES/int/LDS/sandpiper_not_in_Logan/')
+    parser.add_argument('--levels', default=2)
+    parser.add_argument('--match-filename', action='store_true')
     args = parser.parse_args()
 
-    dirs = list()
-    for path in glob.glob(os.path.join(args.root, '*'), recursive=True):
-        name = os.path.basename(path)
-        if os.path.isdir(path) and (re.search(args.pattern, name) is not None):
-            dirs.append(path)
+    if not args.match_filename:
+        dirs = list()
+        dir_pattern = [args.root] + ['*'] * args.levels
+        dir_pattern = os.path.join(*dir_pattern)
+        # print(list(glob.glob(dir_pattern, recursive=True)))
+        for path in glob.glob(dir_pattern, recursive=True):
+            dir_name = os.path.basename(os.path.normpath(path)) 
+            if os.path.isdir(path) and (re.search(args.pattern, dir_name) is not None):
+                dirs.append(path)
 
-    reads_paths = dict()
-    for dir_path in dirs:
-        reads_path_pattern = os.path.join(dir_path, '**', '*.gz')
-        reads_paths[dir_path] = sorted(list(glob.glob(reads_path_pattern, recursive=True)))
+        paths = dict()
+        for dir_path in dirs:
+            reads_path_pattern = os.path.join(dir_path, '**', '*.gz')
+            paths[dir_path] = sorted(list(glob.glob(reads_path_pattern, recursive=True)))
 
-    print(reads_paths)
+        for dir_path, reads_paths in paths.items():
+            dir_name = os.path.basename(os.path.normpath(dir_path)) 
+            print(f'{dir_name}\t{reads_paths[0]}\t{reads_paths[-1]}')
+    
+    else:
+        reads_path_pattern = [args.root] + ['*'] * args.levels + ['*.gz']
+        reads_path_pattern = os.path.join(*reads_path_pattern)
+        reads_paths = sorted(list(glob.glob(reads_path_pattern, recursive=True)))
+        assert len(reads_path_pattern) == 2, f'Expected two matching reads paths, got: {reads_paths}'
+        print(f'{reads_paths[0]}\t{reads_paths[-1]}')
